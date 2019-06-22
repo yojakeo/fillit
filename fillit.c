f6472aa96f77ca6b710373ad1d3839c00149ff69
Jared Parbs <japarbs@e1z2r3p40.42.us.org>
2019-06-22 04:48:58 -0700

    libft update and fillit planning

--- fillit.c
+++ fillit.c
@@ -6,6 +6,28 @@
 /*   By: japarbs <japarbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/21 00:26:48 by japarbs           #+#    #+#             */
-/*   Updated: 2019/06/21 04:01:20 by japarbs          ###   ########.fr       */
+/*   Updated: 2019/06/22 04:47:24 by japarbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	fillit()
+
+
+/*
+**		Sudo code / function planning
+**	Fillit finds the smallest possible square for up to 26 tetrimenos to fit
+**	into via an algorithm.
+**
+**		Planned modules
+**	Function to take GNL's **line and converts & confirms pieces.
+**
+**	Algorithm to make a piece fit within current square.
+**
+**	To save cycles. Before algorithm starts it'll calculate the smallest
+**	possible square within (pieces x 4) rather than starting from 2x2.
+**
+**	module to display square and the lettered pieces in the square.
+**
+**	Linked list OR map to keep track of pieces.
+**	
+*/
