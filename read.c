5c549361d9d0a05eac2ea59b8d06b8d9240088bd
Jared Parbs <japarbs@e1z1r8p20.42.us.org>
2019-08-02 18:23:36 -0700

    FULLY WORKING!

--- read.c
+++ read.c
@@ -6,33 +6,11 @@
 /*   By: japarbs <japarbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 19:49:05 by jetownle          #+#    #+#             */
-/*   Updated: 2019/08/02 02:34:31 by japarbs          ###   ########.fr       */
+/*   Updated: 2019/08/02 18:17:54 by japarbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-
-/*
-**	Takes the given fd and the calculated size of the lines to be read.
-**	using GNL to push into the allocated double pointer.
-**	Returning the read lines.
-*/
-
-// char	**ft_readfd(int fd, size_t size)
-// {
-// 	char	**gnlread;
-// 	char	**temp;
-
-// 	if (!(gnlread = (char **)malloc(sizeof(char *) * (size + 1))))
-// 		return (NULL);
-// 	temp = gnlread;s
-// 	ft_bzero(temp, (size + 1));
-// 	temp = gnlread;
-// 	while (get_next_line(fd, temp) == 1)
-// 		++(temp);
-// 	*temp = NULL;
-// 	return (gnlread);
-// }
 
 /*
 **	Takes an FD and reads the lines via GNL, pushed line is stored in Holder.
@@ -40,13 +18,13 @@
 **	The contents are pushed to the new double pointer.
 */
 
-char **ft_readfd(int fd)
+char	**ft_readfd(int fd)
 {
 	char	**gnlread;
 	char	**temp;
 	char	*holder;
-	size_t	count;
 	size_t	i;
+	size_t	count;
 
 	if (fd < 0)
 		return (NULL);
@@ -54,19 +32,14 @@ char **ft_readfd(int fd)
 	gnlread = NULL;
 	while (get_next_line(fd, &holder) == 1)
 	{
-		printf("holder: %s\n", holder); //testing holder
 		i = 0;
 		temp = gnlread;
-		if (!(gnlread = (char **)ft_memalloc(sizeof(char *) * (++count + 1))))
+		if (!(gnlread = (char **)ft_memalloc(sizeof(char *) * (++count + 2))))
 			return (NULL);
 		if (count > 1)
 			while (temp[++i - 1])
 				gnlread[i - 1] = temp[i - 1];
 		gnlread[count - 1] = holder;
 	}
-	//testing output
-	i = 0;
-	while (i <= count)
-		printf("%s\n", gnlread[i++]);
 	return (gnlread);
 }
